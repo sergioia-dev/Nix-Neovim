0901c3339af6ee88763a5909cbffb63201094f88
sergioia-dev <sergioidarraga2110@gmail.com>
2026-08-26 13:26:01 -0500

    update

--- configuration/lua/plugins/kulala.lua
+++ configuration/lua/plugins/kulala.lua
@@ -1,9 +1,6 @@
 require("kulala").setup({
-	-- global_keymaps = false, -- Set to true to enable global keymaps
-	-- global_keymaps_prefix = "<leader>R",
-	-- kulala_keymaps = "",
-	-- kulala_keymaps_prefix = "",
-	-- default_view = "body", -- Default view when response opens
+	global_keymaps = true, -- Set to true to enable global keymaps
+	-- global_keymaps_prefix = "<leader>r",
+	-- kulala_keymaps = "<leader>r",
 	display_mode = "float",
-	-- default_env = "default", -- Default environment
 })
